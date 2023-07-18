2332e987e7e1a5bfaf6721463666dd9995b5d915
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2023-07-18 08:14:33 +0200

    v0

--- src/chg-package/change_machine_package/algorithms.py
+++ src/chg-package/change_machine_package/algorithms.py
@@ -38,6 +38,38 @@ def greedy_change(
     raise ChangeImpossibleError(amount, coins)
 
 
+def _get_change_making_matrix(set_of_coins, r: int):
+    M = [[0 for _ in range(r + 1)] for _ in range(len(set_of_coins) + 1)]
+    for i in range(1, r + 1):
+        M[0][i] = float("inf")  # By default there is no way of making change
+    return M
+
+
+def dynamic_programming_change(
+    coins: list[int],
+    amount: int,
+) -> list[int]:
+    M = _get_change_making_matrix(coins, amount)
+    for c, coin in enumerate(coins, 1):
+        for r in range(1, amount + 1):
+            # Just use the coin
+            if coin == r:
+                M[c][r] = 1
+            # coin cannot be included.
+            # Use the previous solution for making r,
+            # excluding coin
+            elif coin > r:
+                M[c][r] = M[c - 1][r]
+            # coin can be used.
+            # Decide which one of the following solutions is the best:
+            # 1. Using the previous solution for making r (without using coin).
+            # 2. Using the previous solution for making r - coin (without
+            #      using coin) plus this 1 extra coin.
+            else:
+                M[c][r] = min(M[c - 1][r], 1 + M[c][r - coin])
+    return M[-1][-1]
+
+
 def count_items(items: list[int]) -> dict[int, int]:
     """
     Given a list of items with repetition, return the dict mapping
